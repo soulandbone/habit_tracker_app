19eb829347a6326058db9183021ecea469b71b77
SoulAndBone <soulandbone@gmail.com>
2023-06-05 19:20:02 +0300

    UI improvement when adding tasks

--- lib/widgets/new_habit_dialog.dart
+++ lib/widgets/new_habit_dialog.dart
@@ -7,34 +7,39 @@ class NewHabitDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextEditingController nameController = TextEditingController();
     return AlertDialog(
-      actions: [
-        MaterialButton(
-          onPressed: () {},
-          color: Colors.black,
-          child: const Text(
-            'Save',
-            style: TextStyle(color: Colors.white),
+        actions: [
+          MaterialButton(
+            onPressed: () {},
+            color: Colors.black,
+            child: const Text(
+              'Save',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        MaterialButton(
-          onPressed: () {
-            cancel(context);
-          },
-          color: Colors.black,
-          child: const Text(
-            'Cancel',
-            style: TextStyle(color: Colors.white),
+          MaterialButton(
+            onPressed: () {
+              cancel(context);
+            },
+            color: Colors.black,
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-      ],
-      content: TextField(
-        decoration: const InputDecoration(hintText: 'Enter name of the task'),
-        controller: nameController,
-      ),
-    );
+        ],
+        content: TextField(
+          decoration: const InputDecoration(
+            hintText: 'Enter name of the task',
+            border: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: Colors.black)),
+          ),
+          controller: nameController,
+        ));
   }
 
-  void cancel(BuildContext context) {
+  void cancel(
+    BuildContext context,
+  ) {
     Navigator.of(context).pop();
   }
 }
