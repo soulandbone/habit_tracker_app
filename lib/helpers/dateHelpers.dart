4a79268c33905e63713d65835eb0fa974dd7af5f
SoulAndBone <soulandbone@gmail.com>
2023-06-12 14:03:44 +0300

    minor change in data formula, was working ok, but it wasnt doing what i wanted it to do

--- lib/helpers/dateHelpers.dart
+++ lib/helpers/dateHelpers.dart
@@ -5,15 +5,17 @@ class DateHelpers {
     String day = date.substring(6, 8);
 
     if (day.startsWith('0')) {
-      day = day.substring(0, 2);
+      day = day.substring(1, 2);
     }
     if (month.startsWith('0')) {
-      month = month.substring(0, 2);
+      month = month.substring(1, 2);
     }
 
     DateTime dateTime =
         DateTime(int.parse(year), int.parse(month), int.parse(day));
-
+    print('String year is $year');
+    print('String month is $month');
+    print('String day is $day');
     return dateTime;
   }
 }
